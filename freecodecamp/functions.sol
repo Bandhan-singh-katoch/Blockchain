941e843ce61f1050701fdb2d227997e91b53b94e
Bandhan singh <parbhatbandhan@gmail.com>
2022-09-20 18:17:48 +0530

    Create functions.sol

---
+++ freecodecamp/functions.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+// types and functions
+
+pragma solidity ^0.8.8;
+
+contract SimpleStorage{
+    uint256 public favouriteNumber;
+
+    function store(uint256 _favouriteNumber) public{
+        favouriteNumber = _favouriteNumber;
+    }
+
+    // pure and view doesn't costs gas but if a gas calling function calls them then it will cost gas
+
+    function retrieve() public view returns(uint256){         // view and pure function disallows modification of state
+        return favouriteNumber;                               // so we couldn't update our favouriteNumber
+    }
+}
