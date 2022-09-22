4ee4b3370f44c7b57e3c2ebded188eb88a38c476
Remix Workspace <>
2022-09-22 12:44:01 +0530

    Storage factory file

---
+++ freecodecamp/SimpleStorage.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+// array, structure, mapping...
+
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
+    }
+
+    //calldata: temporary variable that can't be modified ,
+    //memory: temporary variable that can be modified and Storage: permanent variable that can be modified
+
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+   
+}
