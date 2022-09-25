7f3dbbdc02585aa7efbf50c6274ceacb4b56bbec
Remix Workspace <>
2022-09-25 19:50:25 +0530

    managed files

---
+++ freecodecamp/Remix_Storage_Factory/ExtraStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Inheritance in solidity
+
+pragma solidity ^0.8.8;
+import './SimpleStorage.sol';
+
+contract ExtraStorage is SimpleStorage{
+    function store(uint256 _favoriteNumber) public override{
+        favoriteNumber = _favoriteNumber+5;
+    }
+}
