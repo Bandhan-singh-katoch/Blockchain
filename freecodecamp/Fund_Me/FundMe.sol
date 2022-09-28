9b27ba9a74d573376378d4256675f3f43dc8205f
Remix Workspace <>
2022-09-28 13:39:14 +0530

    created library and and added withdraw function

--- freecodecamp/Fund_Me/FundMe.sol
+++ freecodecamp/Fund_Me/FundMe.sol
@@ -1,33 +1,28 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.8;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;
 
     uint256 public minUsd = 50*1e18;
+    address[] public funders;
+    mapping(address=>uint256) public addressToAmountFunded;
 
     function fund() public payable{
-        require(getConversionRate(msg.value) >= minUsd, "You need to send more ETH"); // 1e18 = 1eth      // 18 decimal places  
+        require(msg.value.getConversionRate() >= minUsd, "You need to send more ETH"); // 1e18 = 1eth      // 18 decimal places  
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public view returns(uint256){
-        //ABI
-        //Address : 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        //ETH in term of usd
-        return uint256(price * 1e18);
-
-    }
-
-    function getVersion() public view returns (uint256){
-        return AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e).version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount)/1e18;
-        return ethAmountInUsd;
+    function withdraw() public{
+        // filling the arrayfunder value with 0 value
+        for(uint256 funderIndex = 0; funderIndex<funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset the array
+        funders = new address[](0);
     }
 }
